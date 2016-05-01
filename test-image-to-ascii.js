f137d32633a963b76c7f5d6ae0d66219f2c6a298
Chao Wang <cwang8023@gmail.com>
2016-05-01 16:13:12 +0800

    remove unused file

--- test-image-to-ascii.js
+++
@@ -1,5 +0,0 @@
-const imageToAscii = require("image-to-ascii");
-imageToAscii("./test.jpg",{concat: true, px_background: {r:0,g:255,b:255}, colored: true, bg: true, pixels: ['M', ' '], pxWidth: 1}, (err, converted) => {
-    console.log(err || converted);
-});
-
