a90f1464622713186ffe1c399fe0b7aed1180db6
Chao Wang <cwang8023@gmail.com>
2016-05-01 16:54:43 +0800

    still using url due to daocloud can not show qrcode

--- WeChatClient.js
+++ WeChatClient.js
@@ -400,7 +400,7 @@ class WeChatClient {
   printQRCode(uuid) {
     log.info('QR Code Url: https://login.weixin.qq.com/qrcode/' + uuid);
 
-    qrcode.generate('https://login.weixin.qq.com/l/' + uuid);
+    //qrcode.generate('https://login.weixin.qq.com/l/' + uuid);
     //open('https://login.weixin.qq.com/qrcode/' + uuid);
     return new Promise((resolve, reject) => {
       resolve(uuid)
